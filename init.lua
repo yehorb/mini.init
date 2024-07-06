8ae89e0814d6cb7fd55afaa1785de49bbc7e93c2
Yehor Borkov <yborkov@issp.com>
2024-07-07 00:42:25 +0300

    feat: add lazy loading plugins

--- init.lua
+++ init.lua
@@ -62,4 +62,16 @@ now(function()
   vim.cmd([[colorscheme tokyonight]])
 end)
 
+-- [[ Lazy loading plugins ]]
+
+local augroup = vim.api.nvim_create_augroup("Init", {})
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+  group=augroup,
+  callback=function()
+    add({source="max397574/better-escape.nvim"})
+    require("better_escape").setup()
+  end
+})
+
 -- vim: ts=2 sts=2 sw=2 et
