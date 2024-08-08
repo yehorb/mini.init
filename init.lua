2a3a9823e4fd08dd2d382d7f594c626fa07d0404
Yehor Borkov <yehor.borkov@gmail.com>
2024-08-08 17:39:48 +0300

    feat: add mini.deps

--- init.lua
+++ init.lua
@@ -1,1 +1,18 @@
+-- [[ 'mini.nvim' ]]
+-- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    'git', 'clone', '--filter=blob:none',
+    'https://github.com/echasnovski/mini.nvim', mini_path
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
+
+-- Set up 'mini.deps' (customize to your liking)
+require('mini.deps').setup({ path = { package = path_package } })
 -- vim: ts=2 sts=2 sw=2 et
