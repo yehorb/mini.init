d264b3a606635e50e985789b70d739aa4ef5c392
Yehor Borkov <yehor.borkov@gmail.com>
2024-08-08 22:59:23 +0300

    feat: add keybinds to make split navigation easier

--- init.lua
+++ init.lua
@@ -1,6 +1,12 @@
 pcall(vim.loader.enable)
 
 -- [[ Basic Keymaps ]]
+-- Keybinds to make split navigation easier.
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+
 vim.keymap.set({ 'n', 'v' }, 'Y', '"+y', { desc = 'Yank into the OS clipboard' })
 
 -- [[ Basic Autocommands ]]
