bb04f8e98f94636877a960133b46b03aa29dd315
Yehor Borkov <yehor.borkov@gmail.com>
2024-08-08 21:27:46 +0300

    feat: load mini.diff and mini.git to simplify change management

--- init.lua
+++ init.lua
@@ -16,11 +16,19 @@ end
 -- Set up 'mini.deps' (customize to your liking)
 require('mini.deps').setup({ path = { package = path_package } })
 
-add, now = MiniDeps.add, MiniDeps.now
+add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
 -- [[ Step one - load plugins with UI necessary to make initial screen draw ]]
 now(function()
   add 'shaunsingh/nord.nvim'
   vim.cmd 'colorscheme nord'
 end)
+
+-- [[ Step two - load other plugins ]]
+-- `MiniDeps.later()` schedules code to be safely executed later.
+-- `later(function() require(...).setup() end)` correctly postpones module loading.
+-- `later(require(...).setup)` does not postpone module loading, but only postpones `setup()`.
+later(function() require('mini.diff').setup() end)
+later(function() require('mini.git').setup() end)
+
 -- vim: ts=2 sts=2 sw=2 et
