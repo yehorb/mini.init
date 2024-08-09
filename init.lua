163bc0cfe05b2ff375f9c46fd0b90152ce2bbfae
Yehor Borkov <yehor.borkov@gmail.com>
2024-08-09 17:46:31 +0300

    fix: remove the deprecated `vim.loop`

--- init.lua
+++ init.lua
@@ -49,7 +49,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
 local path_package = vim.fn.stdpath('data') .. '/site/'
 local mini_path = path_package .. 'pack/deps/start/mini.nvim'
-if not vim.loop.fs_stat(mini_path) then
+if not vim.uv.fs_stat(mini_path) then
   vim.cmd('echo "Installing `mini.nvim`" | redraw')
   local clone_cmd = {
     'git', 'clone', '--filter=blob:none',
