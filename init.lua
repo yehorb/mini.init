ed6fbfbc2377d1a58bbed672894dac36e6b7e47c
Yehor Borkov <yehor.borkov@gmail.com>
2024-08-09 18:05:38 +0300

    chore: apply stylua

--- init.lua
+++ init.lua
@@ -7,12 +7,12 @@ vim.opt.listchars = { eol = "␤", tab = "→ ", trail = "␠", nbsp = "␣" }
 -- The initial popup menu is mostly used for preview and sanity checks. As I continue
 -- typing, fewer options become available, allowing me to either select a completion
 -- item or continue typing if I don't see the desired option.
-vim.o.completeopt = 'menuone,preview,noselect'
+vim.o.completeopt = "menuone,preview,noselect"
 
 -- The default value `auto` causes signcolumn to flicker during analysis.
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = "yes"
 
-if vim.uv.os_uname().version:match 'Windows' then
+if vim.uv.os_uname().version:match "Windows" then
   vim.cmd [[
   let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
   let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
@@ -24,21 +24,21 @@ end
 
 -- [[ Basic Keymaps ]]
 -- Keybinds to make split navigation easier.
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 
-vim.keymap.set({ 'n', 'v' }, 'Y', '"+y', { desc = 'Yank into the OS clipboard' })
+vim.keymap.set({ "n", "v" }, "Y", '"+y', { desc = "Yank into the OS clipboard" })
 
 -- [[ Basic Autocommands ]]
 -- Nvim will always call a Lua function with a single table containing information
 -- about the triggered autocommand. This means that if your callback itself takes
 -- an (even optional) argument, you must wrap it in `function() end` to avoid an error.
-vim.api.nvim_create_autocmd('TextYankPost', {
-  group = vim.api.nvim_create_augroup('vimrc', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("vimrc", { clear = true }),
   callback = function() vim.highlight.on_yank() end,
-  desc = 'Briefly highlight yanked text',
+  desc = "Briefly highlight yanked text",
 })
 
 -- [[ Install 'mini.deps' plugin manager ]]
@@ -47,41 +47,44 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- loading modules in the correct order and at the right time is certainly not for everyone, but I
 -- find it much easier to reason about and build upon.
 -- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
-local path_package = vim.fn.stdpath('data') .. '/site/'
-local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+local path_package = vim.fn.stdpath "data" .. "/site/"
+local mini_path = path_package .. "pack/deps/start/mini.nvim"
 if not vim.uv.fs_stat(mini_path) then
-  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  vim.cmd 'echo "Installing `mini.nvim`" | redraw'
   local clone_cmd = {
-    'git', 'clone', '--filter=blob:none',
-    'https://github.com/echasnovski/mini.nvim', mini_path
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/echasnovski/mini.nvim",
+    mini_path,
   }
   vim.fn.system(clone_cmd)
-  vim.cmd('packadd mini.nvim | helptags ALL')
-  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+  vim.cmd "packadd mini.nvim | helptags ALL"
+  vim.cmd 'echo "Installed `mini.nvim`" | redraw'
 end
 
 -- Set up 'mini.deps' (customize to your liking)
-require('mini.deps').setup({ path = { package = path_package } })
+require("mini.deps").setup { path = { package = path_package } }
 
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
 -- [[ Step one - load plugins with UI necessary to make initial screen draw ]]
 now(function()
-  add 'shaunsingh/nord.nvim'
-  vim.cmd 'colorscheme nord'
-  vim.cmd 'highlight! link Whitespace DiagnosticError' -- Highlight nonprinting characters
+  add "shaunsingh/nord.nvim"
+  vim.cmd "colorscheme nord"
+  vim.cmd "highlight! link Whitespace DiagnosticError" -- Highlight nonprinting characters
 end)
 
 -- [[ Step two - load other plugins ]]
 -- `MiniDeps.later()` schedules code to be safely executed later.
 -- `later(function() require(...).setup() end)` correctly postpones module loading.
 -- `later(require(...).setup)` does not postpone module loading, but only postpones `setup()`.
-later(function() require('mini.diff').setup() end)
-later(function() require('mini.git').setup() end)
+later(function() require("mini.diff").setup() end)
+later(function() require("mini.git").setup() end)
 
 later(function()
-  add('neovim/nvim-lspconfig')
-  local lspconfig = require('lspconfig')
+  add "neovim/nvim-lspconfig"
+  local lspconfig = require "lspconfig"
   lspconfig.lua_ls.setup {
     -- The default `root_dir` checks for Lua configuration files, the presence of the `lua/`
     -- directory, and only then for the `.git` directory. It finds my `Projects` directory
@@ -91,12 +94,12 @@ later(function()
     settings = {
       Lua = {
         runtime = {
-          version = 'LuaJIT',
-          path = vim.split(package.path, ';'),
+          version = "LuaJIT",
+          path = vim.split(package.path, ";"),
         },
         workspace = {
           checkThirdParty = false,
-          library = { vim.env.VIMRUNTIME }
+          library = { vim.env.VIMRUNTIME },
         },
         telemetry = {
           enable = false,
@@ -106,7 +109,7 @@ later(function()
   }
   -- Manually trigger `lspconfig` autocommands, as `later()` defers `lspconfig.server.setup()`.
   -- If not triggered, an LSP client will not automatically attach to a buffer.
-  vim.cmd 'doautocmd lspconfig FileType'
+  vim.cmd "doautocmd lspconfig FileType"
 end)
 
 later(function()
