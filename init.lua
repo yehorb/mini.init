2e255c2a1a8bc660f054b3daf81650e971872f55
Yehor Borkov <yehor.borkov@gmail.com>
2024-08-14 08:45:25 +0300

    feat: add oil.nvim

--- init.lua
+++ init.lua
@@ -160,6 +160,17 @@ later(function()
 end)
 
 later(function()
+  add "stevearc/oil.nvim"
+  require("oil").setup()
+  vim.keymap.set("n", "-", "<Cmd>Oil<CR>", { desc = "Open parent directory" })
+  vim.api.nvim_create_user_command("Explore", "Oil <args>", { nargs = "?", complete = "dir" })
+  vim.api.nvim_create_user_command("E", "Explore <args>", { nargs = "?", complete = "dir" })
+  vim.api.nvim_create_user_command("Sexplore", "belowright split | Oil <args>", { nargs = "?", complete = "dir" })
+  vim.api.nvim_create_user_command("Vexplore", "rightbelow vsplit | Oil <args>", { nargs = "?", complete = "dir" })
+  vim.api.nvim_create_user_command("Texplore", "tabedit % | Oil <args>", { nargs = "?", complete = "dir" })
+end)
+
+later(function()
   -- Manually trigger `lspconfig` autocommands, as `later()` defers `lspconfig.server.setup()`.
   -- If not triggered, an LSP client will not automatically attach to a buffer.
   -- https://github.com/echasnovski/mini.nvim/issues/689#issuecomment-1939509494
