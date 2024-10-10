61fe747da96285de228ac752d41a0241ed02b032
Yehor Borkov <yehor.borkov@gmail.com>
2024-10-10 21:11:15 +0300

    refactor: better reflect my obsidian note formatting needs

--- init.lua
+++ init.lua
@@ -339,21 +339,20 @@ require("lazy").setup {
         note_id_func = function(title)
           local suffix = ""
           if title ~= nil then
-            -- If title is given, transform it into valid file name.
-            suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+            suffix = title:gsub("[^A-Za-z0-9- ]", "")
           else
-            -- If title is nil, just add 4 random uppercase letters to the suffix.
             for _ = 1, 4 do
               suffix = suffix .. string.char(math.random(65, 90))
             end
           end
-          return tostring(os.date "%Y%m%d%H%M%S") .. "-" .. suffix
+          return tostring(os.date "%Y%m%d%H%M%S") .. " " .. suffix
         end,
         ---@param note obsidian.Note
         note_frontmatter_func = function(note)
           -- Add the title of the note as an alias.
-          note:add_alias(string.gsub(note.id, "^[0-9]+-", ""))
           if note.title then note:add_alias(note.title) end
+          -- Add the date of the note as an alias.
+          note:add_alias(string.gsub(note.id, "[^0-9]*$", ""))
 
           local out = { id = note.id, aliases = note.aliases, tags = note.tags }
 
