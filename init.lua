cb35867807c66f80f1342bb15745753f0b06766c
Yehor Borkov <yehor.borkov@gmail.com>
2024-10-10 20:49:58 +0300

    feat: set up obsidian nvim

--- init.lua
+++ init.lua
@@ -325,6 +325,58 @@ require("lazy").setup {
 
     { "preservim/vim-pencil", event = "User prose" },
 
+    {
+      "epwalsh/obsidian.nvim",
+      opts = {
+        workspaces = {
+          {
+            name = "Vault",
+            path = vim.fs.normalize "~/Documents/Obsidian Vault",
+          },
+        },
+        ---@param title string|?
+        ---@return string
+        note_id_func = function(title)
+          local suffix = ""
+          if title ~= nil then
+            -- If title is given, transform it into valid file name.
+            suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+          else
+            -- If title is nil, just add 4 random uppercase letters to the suffix.
+            for _ = 1, 4 do
+              suffix = suffix .. string.char(math.random(65, 90))
+            end
+          end
+          return tostring(os.date "%Y%m%d%H%M%S") .. "-" .. suffix
+        end,
+        ---@param note obsidian.Note
+        note_frontmatter_func = function(note)
+          -- Add the title of the note as an alias.
+          note:add_alias(string.gsub(note.id, "^[0-9]+-", ""))
+          if note.title then note:add_alias(note.title) end
+
+          local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+          -- `note.metadata` contains any manually added fields in the frontmatter.
+          -- So here we just make sure those fields are kept in the frontmatter.
+          if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+            for k, v in pairs(note.metadata) do
+              out[k] = v
+            end
+          end
+
+          return out
+        end,
+      },
+      event = {
+        "BufReadPre " .. vim.fs.normalize "~/Documents/Obsidian Vault" .. "/*.md",
+        "BufNewFile " .. vim.fs.normalize "~/Documents/Obsidian Vault" .. "/*.md",
+      },
+      keys = {
+        { "<Leader>on", "<Cmd>ObsidianNew<CR>" },
+      },
+      version = "*", -- recommended, use latest release instead of latest commit
+    },
 
     {
       "m4xshen/hardtime.nvim",
