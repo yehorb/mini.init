aafb983e2bf45bc82cdcee122ea639f8b600acd8
Yehor Borkov <yehor.borkov@gmail.com>
2024-12-14 12:36:27 +0200

    feat: disable pencil by default for prose mode
    The vim-pencil plugin seems to mess up a lot of perfectly good options
    for me, and more config is spent fixing it that in actually helps.

--- after/ftplugin/prose.lua
+++ after/ftplugin/prose.lua
@@ -4,9 +4,6 @@ function M.init()
   vim.wo.spell = true
   vim.bo.spelllang = "uk,en_us,en_gb" -- Using en spelllang slows downg suggestions immensly
 
-  -- Init pencil first, as it sets some options I want to override
-  vim.fn["pencil#init"] { wrap = "soft" }
-
   vim.wo.conceallevel = 1
   vim.wo.list = true
 
