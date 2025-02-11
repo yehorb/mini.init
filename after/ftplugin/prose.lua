cf1bf731eb33238ae0d9967fcafd9d3527a246ac
Yehor Borkov <yehor.borkov@gmail.com>
2025-02-11 18:01:34 +0200

    feat: replace pencil with custom reimplementation of good parts

--- after/ftplugin/prose.lua
+++ after/ftplugin/prose.lua
@@ -1,6 +1,8 @@
 local M = {}
 
 function M.init()
+  M.micropencil()
+
   vim.wo.spell = true
   vim.bo.spelllang = "uk,en_us,en_gb" -- Using en spelllang slows downg suggestions immensly
 
@@ -12,6 +14,38 @@ function M.init()
   vim.bo.expandtab = true
   vim.bo.tabstop = 2
   vim.bo.shiftwidth = 2
+end
+
+function M.micropencil()
+  vim.cmd [[
+    setl textwidth=0
+    setl wrap
+    setl linebreak
+    setl breakat-=*         " avoid breaking footnote*
+    setl breakat-=@         " avoid breaking at email addresses
+    setl colorcolumn=0      " doesn't align as expected
+    setl breakindent
+    setl display+=lastline
+    setl backspace=indent,eol,start
+
+    setl whichwrap+=<,>,b,s,h,l,[,]
+    aug pencil_cursorwrap
+        au BufEnter <buffer> set virtualedit+=onemore
+        au BufLeave <buffer> set virtualedit-=onemore
+    aug END
+
+    setl wrapmargin=0
+    setl autoindent         " needed by formatoptions=n
+    setl formatoptions+=n   " recognize numbered lists
+    setl formatoptions+=1   " don't break line before 1 letter word
+    setl formatoptions+=t   " autoformat of text (vim default)
+
+    " clean out stuff we likely don't want
+    setl formatoptions-=v   " only break line at blank entered during insert
+    setl formatoptions-=w   " avoid erratic behavior if mixed spaces
+    setl formatoptions-=a   " autoformat will turn on with Insert in HardPencil mode
+    setl formatoptions-=2   " doesn't work with with fo+=n, says docs
+  ]]
 end
 
 function M.focus_current_pane()
