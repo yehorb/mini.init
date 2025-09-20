76deca89bb4176a7ac2f722bc9ac3ef8865cb44b
Yehor Borkov <yehor.borkov@gmail.com>
2025-09-20 17:38:06 +0000

    refactor: create set_virtual_h1 function

--- plugin/oil.lua
+++ plugin/oil.lua
@@ -17,25 +17,29 @@ end
 
 M.ns_id = vim.api.nvim_create_namespace "zk-virtual-h1"
 
-local cd = oil.get_current_dir(0)
-local line_count = vim.api.nvim_buf_line_count(0)
--- Line 1 is the `..` entry
-for lnum = 2, line_count do
-  ---@type nil|oil.Entry
-  local entry = oil.get_entry_on_line(0, lnum)
-  if entry ~= nil then
-    if entry.type == "file" then
-      local filename = vim.fs.joinpath(cd, entry.name)
-      local h1 = M.get_h1(filename)
-      if h1 ~= nil then
-        local opts = {
-          virt_text = { { h1, "NonText" } },
-        }
-        vim.api.nvim_buf_set_extmark(0, M.ns_id, lnum - 1, 0, opts)
+M.set_virtulal_h1 = function()
+  local cd = oil.get_current_dir(0)
+  local line_count = vim.api.nvim_buf_line_count(0)
+  -- Line 1 is the `..` entry
+  for lnum = 2, line_count do
+    ---@type nil|oil.Entry
+    local entry = oil.get_entry_on_line(0, lnum)
+    if entry ~= nil then
+      if entry.type == "file" then
+        local filename = vim.fs.joinpath(cd, entry.name)
+        local h1 = M.get_h1(filename)
+        if h1 ~= nil then
+          local opts = {
+            virt_text = { { h1, "NonText" } },
+          }
+          vim.api.nvim_buf_set_extmark(0, M.ns_id, lnum - 1, 0, opts)
+        end
       end
     end
   end
 end
+
+M.set_virtulal_h1()
 
 return M
 -- vim: ts=2 sts=2 sw=2 et
