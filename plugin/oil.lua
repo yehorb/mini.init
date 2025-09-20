2dc39b1420d9eb71be03ba88a53a97606c42ace2
Yehor Borkov <yehor.borkov@gmail.com>
2025-09-20 17:06:09 +0000

    feat: construct the filename of the current entry

--- plugin/oil.lua
+++ plugin/oil.lua
@@ -1,13 +1,17 @@
 local ok, oil = pcall(require, "oil")
 if not ok then vim.notify("oil.nvim is not available", vim.log.levels.WARN, {}) end
 
+local cd = oil.get_current_dir(0)
 local line_count = vim.api.nvim_buf_line_count(0)
 -- Line 1 is the `..` entry
 for lnum = 2, line_count do
   ---@type nil|oil.Entry
   local entry = oil.get_entry_on_line(0, lnum)
   if entry ~= nil then
-    if entry.type == "file" then vim.print(entry) end
+    if entry.type == "file" then
+      local file = vim.fs.joinpath(cd, entry.name)
+      vim.print(file)
+    end
   end
 end
 -- vim: ts=2 sts=2 sw=2 et
