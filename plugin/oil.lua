33edd7f12914b665cc7139f04ab9851c1b549ff3
Yehor Borkov <yehor.borkov@gmail.com>
2025-09-20 17:03:35 +0000

    fix: start from line 2

--- plugin/oil.lua
+++ plugin/oil.lua
@@ -2,7 +2,8 @@ local ok, oil = pcall(require, "oil")
 if not ok then vim.notify("oil.nvim is not available", vim.log.levels.WARN, {}) end
 
 local line_count = vim.api.nvim_buf_line_count(0)
-for lnum = 1, line_count do
+-- Line 1 is the `..` entry
+for lnum = 2, line_count do
   ---@type nil|oil.Entry
   local entry = oil.get_entry_on_line(0, lnum)
   vim.print(entry)
