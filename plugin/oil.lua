689616172a461685c19c68cea4d021ba172d102e
Yehor Borkov <yehor.borkov@gmail.com>
2025-09-20 17:07:12 +0000

    feat: outline the get_h1 function

--- plugin/oil.lua
+++ plugin/oil.lua
@@ -1,6 +1,10 @@
 local ok, oil = pcall(require, "oil")
 if not ok then vim.notify("oil.nvim is not available", vim.log.levels.WARN, {}) end
 
+---@param filename string
+---@return nil|string
+local function get_h1(filename) return nil end
+
 local cd = oil.get_current_dir(0)
 local line_count = vim.api.nvim_buf_line_count(0)
 -- Line 1 is the `..` entry
@@ -9,8 +13,9 @@ for lnum = 2, line_count do
   local entry = oil.get_entry_on_line(0, lnum)
   if entry ~= nil then
     if entry.type == "file" then
-      local file = vim.fs.joinpath(cd, entry.name)
-      vim.print(file)
+      local filename = vim.fs.joinpath(cd, entry.name)
+      local h1 = get_h1(filename)
+      vim.print(h1)
     end
   end
 end
