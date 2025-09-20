c0da0f821c2fef30f367a788a1a2cb053369b6b3
Yehor Borkov <yehor.borkov@gmail.com>
2025-09-20 17:01:57 +0000

    feat: get oil.Entry on every line in buffer

--- plugin/oil.lua
+++ plugin/oil.lua
@@ -1,3 +1,10 @@
 local ok, oil = pcall(require, "oil")
 if not ok then vim.notify("oil.nvim is not available", vim.log.levels.WARN, {}) end
+
+local line_count = vim.api.nvim_buf_line_count(0)
+for lnum = 1, line_count do
+  ---@type nil|oil.Entry
+  local entry = oil.get_entry_on_line(0, lnum)
+  vim.print(entry)
+end
 -- vim: ts=2 sts=2 sw=2 et
